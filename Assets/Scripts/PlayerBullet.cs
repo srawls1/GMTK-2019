a1ca19cb55ccfcdb9ffa431687943925c982bb75
Lieu Zheng Hong <lieuzhenghong@gmail.com>
2019-08-03 13:41:00 +0100

    add player bullet

---
+++ Assets/Scripts/PlayerBullet.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class PlayerBullet : MonoBehaviour
+{
+	[SerializeField] private float baseMaxSpeed;
+	[SerializeField] private float forceTowardPlayer;
+	[SerializeField] private float speedUpStep;
+	[SerializeField] private GameObject playerCharacter;
+
+	private new Rigidbody2D rigidbody;
+	private float currentMaxSpeed;
+
+	private void Awake()
+	{
+		rigidbody = GetComponent<Rigidbody2D>();
+		currentMaxSpeed = baseMaxSpeed;
+	}
+
+	void Update()
+	{
+        //Debug.Log("updating");
+		Vector2 distFromPlayer = playerCharacter.transform.position - transform.position;
+		Vector2 force = distFromPlayer.normalized * forceTowardPlayer - rigidbody.velocity;
+		rigidbody.AddForce(force);
+		if (rigidbody.velocity.magnitude > currentMaxSpeed)
+		{
+			rigidbody.velocity = rigidbody.velocity.normalized * baseMaxSpeed;
+		}
+	}
+
+    void OnCollisionEnter2D (Collision2D col) {
+        Debug.Log("Hello");
+        Destroy(gameObject);
+    }
+}
