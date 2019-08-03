20180979d5177e4fd0f16e60d3f8c44f0ed6bc75
Spencer Rawls <rawlsspencer@gmail.com>
2019-08-02 21:27:33 -0700

    Implemented basic character movement

--- Assets/Scripts/CharacterController.cs
+++ Assets/Scripts/CharacterController.cs
@@ -1,16 +1,194 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CharacterController : MonoBehaviour {
+[RequireComponent(typeof(Rigidbody2D)), RequireComponent(typeof(Animator))]
+public class CharacterController : MonoBehaviour
+{
+	[Header("Running")]
+	[SerializeField] private float maxSpeed;
+	[SerializeField] private float timeToFullSpeed;
+	[SerializeField] private float timeToStop;
 
-	// Use this for initialization
-	void Start () {
-		
+	[Header("Jumping")]
+	[SerializeField] private float jumpHeight;
+	[SerializeField] private float variableHangTime;
+	[SerializeField] private float coyoteTime;
+	[SerializeField] private float inputQueueTime;
+
+	[Header("Dash")]
+	[SerializeField] private float dashSpeed;
+	[SerializeField] private float dashControlLossDuration;
+	[SerializeField] private float dashDuration;
+	[SerializeField, Tooltip("The length of time that the game pauses when you start a dash")] private float pauseDuration;
+	[SerializeField] private float dashCooldownTime;
+
+	private new Rigidbody2D rigidbody;
+	private Animator animator;
+	private new Collider2D collider;
+	private float acceleration;
+	private float timeLeftGround;
+	private float jumpQueuedUntil;
+	private float lastDash;
+	private bool facingRight = true;
+
+	public bool Charging
+	{
+		get; private set;
+	}
+
+	void Awake()
+	{
+		rigidbody = GetComponent<Rigidbody2D>();
+		animator = GetComponent<Animator>();
+		collider = GetComponent<Collider2D>();
 	}
 	
-	// Update is called once per frame
-	void Update () {
+	void Update()
+	{
+		UpdateIsOnGround();
+		CheckForJump();
+		CheckForDash();
+		ApplyHorizontalAcceleration();
+	}
+
+	private void CheckForDash()
+	{
+		if (lastDash + dashCooldownTime < Time.time && Input.GetButton("Dash")) // We are not in a dash cooldown
+		{
+			Dash();
+		}
 		
+	}
+
+	private void Dash()
+	{
+		acceleration = 0f;
+		lastDash = Time.time;
+		Vector2 direction;
+
+		Vector2 input = new Vector2(Input.GetAxisRaw("Horizontal"), IsOnGround() ? 0f : Input.GetAxisRaw("Vertical"));
+		if (input.magnitude < 0.1f)
+		{
+			direction = facingRight ? Vector2.right : Vector2.left;
+		}
+		else
+		{
+			float angle = Mathf.Atan2(input.y, input.x);
+			angle /= Mathf.PI / 4;
+			angle = Mathf.Round(angle);
+			angle *= Mathf.PI / 4;
+			direction = new Vector2(Mathf.Cos(angle), Mathf.Sin(angle));
+		}
+
+		StartCoroutine(DashRoutine(direction));
+	}
+
+	private IEnumerator DashRoutine(Vector2 direction)
+	{
+		Time.timeScale = 0f;
+		yield return new WaitForSecondsRealtime(pauseDuration);
+		Time.timeScale = 1f;
+
+		Vector2 velocity = direction * dashSpeed;
+		rigidbody.velocity = velocity;
+		Charging = true;
+		animator.SetBool("Charging", true);
+
+		for (float timePassed = 0f; timePassed < dashControlLossDuration; timePassed += Time.deltaTime)
+		{
+			rigidbody.velocity = velocity;
+			Debug.Log(rigidbody.velocity);
+			yield return null;
+		}
+
+		float remainingTime = dashDuration - dashControlLossDuration;
+		for (float timePassed = 0f; timePassed < remainingTime; timePassed += Time.deltaTime)
+		{
+			rigidbody.velocity = velocity;
+			float input = Input.GetAxisRaw("Horizontal");
+			if (input != 0f && direction.x * input <= 0) // There is horizontal input, and it's not in the direction of the dash, so we cancel out of it
+			{
+				break;
+			}
+		}
+
+		Charging = false;
+		animator.SetBool("Charging", false);
+	}
+
+	private void ApplyHorizontalAcceleration()
+	{
+		if (!Charging)
+		{
+			float input = Mathf.Round(Input.GetAxisRaw("Horizontal"));
+			Vector2 velocity = rigidbody.velocity;
+			float time = input == 0f ? timeToStop : timeToFullSpeed;
+
+			velocity.x = Mathf.SmoothDamp(velocity.x, input * maxSpeed, ref acceleration, time);
+			rigidbody.velocity = velocity;
+
+			if (velocity.x > 0.05f)
+			{
+				facingRight = true;
+			}
+			if (velocity.x < -0.05f)
+			{
+				facingRight = false;
+			}
+			animator.SetFloat("Speed", velocity.x);
+		}
+	}
+
+	private void CheckForJump()
+	{
+		if (IsOnGround())
+		{
+			if (Input.GetButtonDown("Jump") || jumpQueuedUntil > Time.time) // Either they just pressed the jump button, or they just landed and a jump was queued
+			{
+				Jump();
+			}
+		}
+		else // Can't jump. Just queue the input in case we're about to land
+		{
+			if (Input.GetButtonDown("Jump"))
+			{
+				jumpQueuedUntil = Time.time + inputQueueTime;
+			}
+		}
+	}
+
+	private bool IsOnGround()
+	{
+		return timeLeftGround + coyoteTime > Time.time; // This means we are either on the ground, or still within the coyote time window
+	}
+
+	private void Jump()
+	{
+		jumpQueuedUntil = Time.time - Time.deltaTime; // Unqueue the jump input by setting it into the past
+		Vector2 velocity = rigidbody.velocity;
+
+		float jumpSpeed = Mathf.Sqrt(2f * jumpHeight * Physics2D.gravity.magnitude);
+		velocity.y = jumpSpeed;
+		rigidbody.velocity = velocity;
+
+		animator.SetTrigger("Jump");
+	}
+
+	private void UpdateIsOnGround()
+	{
+		Vector2 bounds = collider.bounds.extents;
+		// This raycast downward just beyond the extent of the character's collider will check if the character is standing on something
+		if (Physics2D.Raycast(transform.position, Vector2.down, bounds.y + 0.05f).collider != null)
+		{
+			animator.SetBool("OnFloor", true);
+			// Update the time they left the ground to now. If they are not on the ground, this will have been last updated when they left.
+			timeLeftGround = Time.time;
+		}
+		else
+		{
+			animator.SetBool("OnFloor", false);
+		}
 	}
 }
