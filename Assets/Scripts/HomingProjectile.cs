ef9134e51c84226b0a75062aac67e621a6109113
Spencer Rawls <rawlsspencer@gmail.com>
2019-08-02 22:06:59 -0700

    Added homing projectile

---
+++ Assets/Scripts/HomingProjectile.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class HomingProjectile : MonoBehaviour
+{
+	[SerializeField] private float maxSpeed;
+	[SerializeField] private float forceTowardPlayer;
+
+	[SerializeField] private GameObject playerCharacter;
+
+	private new Rigidbody2D rigidbody;
+
+	private void Awake()
+	{
+		rigidbody = GetComponent<Rigidbody2D>();
+	}
+
+	void Update()
+	{
+		Vector2 distFromPlayer = playerCharacter.transform.position - transform.position;
+		Vector2 force = distFromPlayer.normalized * forceTowardPlayer - rigidbody.velocity;
+		rigidbody.AddForce(force);
+		if (rigidbody.velocity.magnitude > maxSpeed)
+		{
+			rigidbody.velocity = rigidbody.velocity.normalized * maxSpeed;
+		}
+	}
+}
