39317fab165f0335affd6e0355b8d820d81c4f08
Lieu Zheng Hong <lieuzhenghong@gmail.com>
2019-08-03 19:07:25 +0100

    Make Line prefab and add player health + bullet colliding script

---
+++ Assets/Scripts/PlayerLives.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerLives : MonoBehaviour
+{
+	[SerializeField] private int baseMaxHealth;
+    [SerializeField] private Rigidbody2D rigidbody;
+    // Start is called before the first frame update
+
+    private int current_health;
+
+    void Start()
+    {
+        int current_health = baseMaxHealth;
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnCollisionEnter2D(Collision2D other) {
+        HomingProjectile homingprojectile = other.collider.GetComponent<HomingProjectile>();
+        if (homingprojectile != null) {
+            Debug.Log("Lost 1 health");
+            current_health -= 1;
+            Destroy(other.gameObject);
+        }
+        // todo probably need to make the 
+        // decrease the health bar
+    }
+}
