f0b5b40d0a94a7516578bf88a6f1680c6e071c93
Lieu Zheng Hong <lieuzhenghong@gmail.com>
2019-08-04 18:03:05 +0100

    Build start menu

---
+++ Assets/Scripts/StartMenu.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartMenu : MonoBehaviour
+{
+
+    public void changeScene(string sceneName) {
+        Application.LoadLevel(sceneName);
+    }
+
+
+    public void startGame() {
+        Debug.Log("Starting game...");
+        // TODO call the scene game
+        changeScene("game");
+    }
+
+}
